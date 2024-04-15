fcc5946ec7baec99d7ed52f7883ccebbe09f8e53
Fernando Soto <fernando.soto@epn.edu.ec>
2024-04-15 10:39:02 -0500

    feat: get youtube videos id from stackbuilders channel

---
+++ src/search/videoId.ts
@@ -0,0 +1,31 @@
+import client from "../api/client.js"
+import {
+    CHANNEL_ID,
+    MAX_RESULTS,
+    PART,
+    FIELDS,
+    TYPE,
+    VIDEO_DURATION,
+} from "../utils/constants.js"
+
+async function getVideosId(): Promise<string[]> {
+    try {
+        const response = await client.search.list({
+            part: PART,
+            channelId: CHANNEL_ID,
+            fields: FIELDS,
+            maxResults: MAX_RESULTS,
+            type: TYPE,
+            videoDuration: VIDEO_DURATION,
+        })
+        return response.data.items
+            ? (response.data.items
+                  .map((item) => item.id?.videoId)
+                  .filter((videoId) => typeof videoId === "string") as string[])
+            : []
+    } catch (err) {
+        throw new Error("Error fetching videos from the API: " + err.message)
+    }
+}
+
+export default getVideosId
---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+const CHANNEL_ID = "UCNLpfsZSr1XCyYyB9pdjoQQ"
+const MAX_RESULTS = 50
+const PART = "snippet"
+const FIELDS = "items/id/videoId"
+const TYPE = "video"
+const VIDEO_DURATION = "any"
+
+export { CHANNEL_ID, MAX_RESULTS, PART, FIELDS, TYPE, VIDEO_DURATION }

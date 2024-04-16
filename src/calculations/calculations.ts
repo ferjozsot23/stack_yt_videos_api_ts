463a5af9cee25f2cb1f4bf40ad4aaed4a1d93f88
ferjozsot23 <fernando.soto@epn.edu.ec>
2024-04-16 11:51:51 -0500

    refactor: calculations and videoMetadata

--- src/calculations/calculations.ts
+++ src/calculations/calculations.ts
@@ -1,39 +1,40 @@
-import { Video } from "../models/Video.ts"
+import { Video } from "../models/Video.ts";
 
 const getTopLikedVideos = (data: Video[]) =>
-    Array.isArray(data) && data
-        ? data
-              .sort(
-                  (videoA: Video, videoB: Video) =>
-                      videoB.likeCount - videoA.likeCount
-              )
-              .slice(0, 5)
-        : []
+  Array.isArray(data)
+    ? data
+        .sort(
+          (videoA: Video, videoB: Video) => videoB.likeCount - videoA.likeCount
+        )
+        .slice(0, 5)
+    : [];
 
 const getNewestVideos = (data: Video[]) =>
-    Array.isArray(data) && data
-        ? data
-              .sort(
-                  (videoA: Video, videoB: Video) =>
-                      new Date(videoB.publishedAt).getTime() -
-                      new Date(videoA.publishedAt).getTime()
-              )
-              .slice(0, 5)
-        : []
+  Array.isArray(data)
+    ? data
+        .sort(
+          (videoA: Video, videoB: Video) =>
+            new Date(videoB.publishedAt).getTime() -
+            new Date(videoA.publishedAt).getTime()
+        )
+        .slice(0, 5)
+    : [];
 
 const getTotalLikes = (data: Video[]) =>
-    Array.isArray(data) && data
-        ? data.reduce(
-              (total: number, video: Video) => total + video.likeCount,
-              0
-          )
-        : 0
+  Array.isArray(data)
+    ? data.reduce((total: number, video: Video) => total + video.likeCount, 0)
+    : 0;
 
 const getTopViewedVideos = (data: Video[]) =>
-    Array.isArray(data) && data
-        ? data
-              .sort((videoA, videoB) => videoB.viewCount - videoA.viewCount)
-              .slice(0, 5)
-        : []
+  Array.isArray(data)
+    ? data
+        .sort((videoA, videoB) => videoB.viewCount - videoA.viewCount)
+        .slice(0, 5)
+    : [];
 
-export { getTopLikedVideos, getNewestVideos, getTotalLikes, getTopViewedVideos }
+export {
+  getTopLikedVideos,
+  getNewestVideos,
+  getTotalLikes,
+  getTopViewedVideos,
+};
